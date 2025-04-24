506e910e3884bfe925c01597cb36c21d301ec103
vuongkienthanh <thanhstardust@outlook.com>
2025-04-24 15:32:25 +0700

    zz

---
+++ packages/cch_his_auto_lib/src/cch_his_auto_lib/tasks/chitietnguoibenhnoitru/tab_todietri/__init__.py
@@ -0,0 +1,5 @@
+from cch_his_auto_lib.driver import Driver
+from cch_his_auto_lib.tasks.chitietnguoibenhnoitru import _logger, ACTIVE_PANE
+
+TAB_NUMBER = 3
+_logger = _logger.getChild("tab_todieutri")
