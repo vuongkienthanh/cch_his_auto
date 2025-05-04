1596b1f11b00db56fd7c55fccbf0b1cee0d3ea60
vuongkienthanh <thanhstardust@outlook.com>
2025-05-04 22:02:40 +0700

    add tab hoi chan

--- packages/cch_his_auto_lib/src/cch_his_auto_lib/tasks/dropdown_menu.py
+++ packages/cch_his_auto_lib/src/cch_his_auto_lib/tasks/dropdown_menu.py
@@ -1,16 +1,18 @@
 from selenium.webdriver import ActionChains, Keys
 from cch_his_auto_lib.driver import Driver
 
+DROPDOWN_CSS = ".ant-select-dropdown:not(.ant-select-dropdown-hidden)"
+
 
 def close_dropdown(driver: Driver):
-    ele = driver.waiting(".ant-select-dropdown", "dropdown")
+    ele = driver.waiting(DROPDOWN_CSS, "dropdown")
     ActionChains(driver).send_keys_to_element(ele, Keys.ESCAPE).perform()
 
 
 def count_item_dropdown(driver: Driver) -> int:
-    driver.waiting(".ant-select-dropdown .ant-select-item", "dropdown")
-    return len(driver.find_all(".ant-select-dropdown .ant-select-item"))
+    driver.waiting(f"{DROPDOWN_CSS} .ant-select-item", "dropdown")
+    return len(driver.find_all(f"{DROPDOWN_CSS} .ant-select-item"))
 
 
 def select_item_dropdown(driver: Driver, i: int):
-    driver.find_all(".ant-select-dropdown .ant-select-item")[i].click()
+    driver.find_all(f"{DROPDOWN_CSS} .ant-select-item")[i].click()
